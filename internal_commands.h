511ec7aae89719a7c1a125b92a0de65b7f98e07e
andrecostamarques <andre.costa.marques@hotmail.com>
2024-05-01 13:10:28 -0300

    criação da branch external_functions e criação do arquivo .c do ls

--- internal_commands.h
+++ internal_commands.h
@@ -16,6 +16,11 @@ void call_internal_command(char command[]) {
     }
 
     splitted_command[i] = NULL;
+
+    for(int j = 0; splitted_command[j] != NULL; j++){
+        char* a = splitted_command[j];
+        printf("%c\n",a[0]); 
+    }
     
 
     if (strcmp(splitted_command[0], "exit") == 0) {
